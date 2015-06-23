4c1055356e7eae352c0857841b3f90bbb6ca31ec
Shardic <marco.geils@freenet.de>
2015-06-23 14:36:46 +0200

    draw plaine in cpp

--- GameField.cpp
+++ GameField.cpp
@@ -5,3 +5,14 @@ GameField::GameField() {
 }
 GameField::~GameField(){
 }
+//zeichnet das Grundlegende Spielfeld
+void drawPlaine(float fieldSize) {
+	glBegin(GL_QUADS);
+	glNormal3f(0.0f, 0.0f, -1.0f);
+	glVertex3f(fieldSize/2.0f, fieldSize/2.0f, -fieldSize/2.0f);
+	glVertex3f(fieldSize/2.0f, -fieldSize/2.0f, -fieldSize/2.0f);
+	glVertex3f(-fieldSize/2.0f, -fieldSize/2.0f, -fieldSize/2.0f);
+	glVertex3f(-fieldSize/2.0f, fieldSize/2.0f, -fieldSize/2.0f);
+	glEnd();
+}
+
